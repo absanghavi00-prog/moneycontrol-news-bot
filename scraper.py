4926ee73b777064a22e2f8c73f623bfa8035b418
absanghavi00-prog <absanghavi00@gmail.com>
2026-06-17 17:12:12 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,50 @@
 import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.moneycontrol.com/news/business/stocks/"
 TOPIC = "akshit-moneycontrol-stocks"
 
-print("Sending test notification...")
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
-r = requests.post(
-    f"https://ntfy.sh/{TOPIC}",
-    data="Hello from GitHub Actions!".encode("utf-8")
+print("Downloading page...")
+
+r = requests.get(
+    URL,
+    headers=headers,
+    timeout=20
 )
 
-print("Status code:", r.status_code)
-print("Response:", r.text)
+print("Status:", r.status_code)
 
-print("Done")
+soup = BeautifulSoup(r.text, "html.parser")
+
+count = 0
+
+for a in soup.find_all("a", href=True):
+
+    href = a["href"]
+
+    title = a.get_text(strip=True)
+
+    if len(title) < 30:
+        continue
+
+    if "moneycontrol.com" not in href:
+        continue
+
+    print(title)
+
+    requests.post(
+        f"https://ntfy.sh/{TOPIC}",
+        data=f"{title}\n\n{href}".encode("utf-8")
+    )
+
+    count += 1
+
+    # send only first 5 for testing
+    if count >= 5:
+        break
+
+print("Sent", count, "notifications")
